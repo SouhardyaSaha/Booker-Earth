53ef538f33c6e6664d7dc09ba6d254e16976cb93
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2019-11-05 17:05:27 +0600

    searchbox moved from the code and included in include directory

---
+++ resources/views/include/searchbox.blade.php
@@ -0,0 +1,18 @@
+<form class="form-horizontal" method="GET" action="{{ url('book-posts') }}">
+        {{ csrf_field() }}
+        
+        <div class="form-group{{ $errors->has('search') ? ' has-error' : '' }}">
+                <label for="search" class="col-md-3 mr-0 control-label"><i class="fa fa-search fa-2x"></i></label>
+            
+            <div class="col-md-6" >
+                <input id="search" type="text"  class="form-control" name="search" placeholder="Search" value = {{$searchInput}}> 
+                {{-- value="{{ old('search') }}" required autofocus --}}
+
+                @if ($errors->has('search'))
+                    <span class="help-block">
+                        <strong>{{ $errors->first('search') }}</strong>
+                    </span>
+                @endif
+            </div>
+        </div>
+</form>
