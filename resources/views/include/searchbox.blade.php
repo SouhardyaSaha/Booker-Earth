dbc9a0bde1309e414778b71d5f86b8a65059fa1a
Souhardya Saha <43247008+SouhardyaSaha@users.noreply.github.com>
2019-11-05 21:59:41 +0600

    csrf token removed from search form

--- resources/views/include/searchbox.blade.php
+++ resources/views/include/searchbox.blade.php
@@ -1,5 +1,5 @@
 <form class="form-horizontal" method="GET" action="{{ url('book-posts') }}">
-        {{ csrf_field() }}
+        
         
         <div class="form-group{{ $errors->has('search') ? ' has-error' : '' }}">
                 <label for="search" class="col-md-3 mr-0 control-label"><i class="fa fa-search fa-2x"></i></label>
