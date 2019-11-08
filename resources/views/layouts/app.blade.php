e368b2f6659e4fc9af447dd0cf79aef6e1556586
Souhardya Saha <souhardya24@student.sust.edu>
2019-11-08 14:00:43 +0600

    book-post link added for guests in the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,7 @@
                     <ul class="nav navbar-nav">
                         @guest
                             <li><a href="{{ url('book-requests') }}">Book Requests</a></li>
+                            <li><a href="{{ url('book-posts') }}">Book Posts</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
