ecef4dd8d0638a7cf1597519443ace85dcb1271b
Mahdi <Mahdi Hasan>
2020-01-20 19:48:53 +0600

    User current status chart added

--- resources/views/include/chart.blade.php
+++ resources/views/include/chart.blade.php
@@ -1,80 +1,70 @@
-@extends('layouts.app')
 
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  {{-- //<title>My Chart.js Chart</title> --}}
-</head>
-<body>
-  <div class="container" style="width: 50%">
-    <canvas id="myChart"></canvas>
-  </div>
-
-  <script>
-    let myChart = document.getElementById('myChart').getContext('2d');
-
-    // Global Options
-    Chart.defaults.global.defaultFontFamily = 'Lato';
-    Chart.defaults.global.defaultFontSize = 15;
-    Chart.defaults.global.defaultFontColor = '#777';
-
-    let massPopChart = new Chart(myChart, {
-      type:'doughnut', // bar, horizontalBar, pie, line, , radar, polarArea
-      data:{
-        labels:['TotalBookPosts', 'TotalBookRequest', 'SentMessages', 'ReceivedMessages'],
-        datasets:[{
-          label:'Population',
-          data:[
-            {{$totalBookPosts}},
-            {{$totalBookRequests}},
-            {{$messages['totalSentMessages']}},
-            {{$messages['totalReceivedMessages']}},                            
-          ],
-          //backgroundColor:'green',
-          backgroundColor:[
-            'rgba(255, 99, 132, 0.6)',
-            'rgba(54, 162, 235, 0.6)',
-            'rgba(255, 206, 86, 0.6)',
-            'rgba(75, 192, 192, 0.6)',
-          ],
-          borderWidth:1,
-          borderColor:'#777',
-          hoverBorderWidth:3,
-          hoverBorderColor:'#000'
-        }]
-      },
-      options:{
-        title:{
-          display:true,
-          text:'Largest Cities In Massachusetts',
-          fontSize:25
-        },
-        legend:{
-          display:true,
-          position:'right',
-          labels:{
-            fontColor:'#000'
-          }
-        },
-        layout:{
-          padding:{
-            left:100,
-            right:100,
-            bottom:80,
-            top:0
-          }
-        },
-        tooltips:{
-          enabled:true       
-        }
-      }
-    });
-  </script>
+<div class="container pull-left" style="width: 35%" >
+  <canvas id="myChart"></canvas>
 </div>
-</body>
-</html>
+{{-- {{$totalBookPosts}} --}}
+@section('script')
+  
+<script>
+  let myChart = document.getElementById('myChart').getContext('2d');
+  // Global Options
+  Chart.defaults.global.defaultFontFamily = 'Lato';
+  Chart.defaults.global.defaultFontSize = 15;
+  Chart.defaults.global.defaultFontColor = '#777';
+  
+  let massPopChart = new Chart(myChart, {
+    type:'doughnut', // bar, horizontalBar, pie, line, , radar, polarArea
+    data:{
+      labels:['Book Posts', 'Book Request', 'Sent Messages', 'Received Messages'],
+      datasets:[{
+        label:'a',
+        data:[         
+          {{$totalBookPosts}},
+          {{$totalBookRequests}},
+          {{$messages['totalReceivedMessages']}},
+          {{$messages['totalSentMessages']}}                             
+        ],
+        backgroundColor:'green',
+        backgroundColor:[
+          'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+        ],
+        borderWidth:1,
+        borderColor:'#777',
+        hoverBorderWidth:3,
+        hoverBorderColor:'#000'
+      }]
+    },
+    options:{
+    title:{
+      display:true,
+      text:'Your Current Status',
+      fontSize:20,
+      position:'top'
+    },
+    legend:{
+      display:true,
+      position:'right',
+      labels:{
+        fontColor:'#000'
+      }
+    },
+    layout:{
+      padding:{
+        left:0,
+        right:-5,
+        bottom:0,
+        top:0
+      }
+    },
+    tooltips:{
+      enabled:true       
+    }
+  }
+});
 
+</script>
+
+@endsection
